53c9072769059ad6ad932c6b7900d923375528f2
uhgfel <uhgfel@gmail.com>
2022-08-31 11:00:39 +0300

    liveStream experimentation

--- SampleCode-V5/android-sdk-v5-sample/module-common/src/main/java/dji/sampleV5/modulecommon/DataStreamingController.java
+++ SampleCode-V5/android-sdk-v5-sample/module-common/src/main/java/dji/sampleV5/modulecommon/DataStreamingController.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.telecom.Call;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.method.ScrollingMovementMethod;
@@ -44,6 +45,7 @@ import dji.sdk.keyvalue.key.BatteryKey;
 //import dji.common.airlink
 //import com.dji.sdk.sample.demo.camera;
 //import dji.v5.manager.interfaces;
+import dji.sdk.keyvalue.value.airlink.VideoSourceEntity;
 import dji.v5.manager.video.channel.BaseVideoChannel;
 
 import dji.sdk.keyvalue.value.airlink.*;
@@ -67,6 +69,7 @@ import dji.v5.manager.datacenter.MediaDataCenter;
 import dji.v5.manager.interfaces.IVideoStreamManager;
 import dji.v5.manager.video.DualChannelVideoStreamManagerDelegate;
 import dji.v5.manager.video.channel.BaseVideoChannel;
+import dji.v5.manager.interfaces.IMediaManager;
 
 import dji.sdk.keyvalue.value.common.LocationCoordinate3D;
 import dji.sdk.keyvalue.value.common.Velocity3D;
@@ -77,6 +80,111 @@ import dji.v5.utils.common.DjiSharedPreferencesManager;
 import dji.v5.utils.common.LogUtils;
 import dji.sdk.keyvalue.key.KeyTools;
 import java.text.MessageFormat;
+
+import dji.sampleV5.modulecommon.models.LiveStreamVM;
+import dji.v5.common.callback.CommonCallbacks;
+import androidx.lifecycle.MutableLiveData;
+import dji.v5.common.callback.CommonCallbacks;
+import dji.v5.common.error.IDJIError;
+import dji.v5.common.video.channel.VideoChannelState;
+import dji.v5.common.video.channel.VideoChannelType;
+import dji.v5.manager.datacenter.MediaDataCenter;
+import dji.v5.manager.interfaces.ILiveStreamManager;
+import dji.v5.manager.datacenter.livestream.*;
+import dji.v5.manager.datacenter.livestream.settings.AgoraSettings;
+import dji.v5.manager.datacenter.livestream.settings.GB28181Settings;
+import dji.v5.manager.datacenter.livestream.settings.RtmpSettings;
+import dji.v5.manager.datacenter.livestream.settings.RtspSettings;
+import dji.v5.manager.datacenter.video.VideoStreamManager;
+import dji.v5.utils.common.ContextUtil;
+import dji.v5.utils.common.DjiSharedPreferencesManager;
+import dji.sampleV5.modulecommon.models.VideoPlayVM;
+
+
+import dji.v5.manager.datacenter.MediaDataCenter;
+
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.os.*;
+import android.view.*;
+import android.widget.Button;
+import androidx.appcompat.app.AlertDialog;
+import androidx.lifecycle.ViewModelProvider;
+import dji.sampleV5.modulecommon.R;
+import dji.sampleV5.modulecommon.models.MultiVideoChannelVM;
+import dji.sampleV5.modulecommon.models.VideoChannelVM;
+import dji.sampleV5.modulecommon.models.VideoChannelVMFactory;
+import dji.sampleV5.modulecommon.util.ToastUtils;
+import dji.v5.common.callback.CommonCallbacks;
+import dji.v5.common.error.IDJIError;
+import dji.v5.common.video.channel.VideoChannelState;
+import dji.v5.common.video.channel.VideoChannelType;
+import dji.v5.common.video.decoder.*;
+import dji.v5.common.video.interfaces.*;
+import dji.v5.common.video.stream.StreamSource;
+import dji.v5.utils.common.ContextUtil;
+import dji.v5.utils.common.DiskUtil;
+import dji.v5.utils.common.LogUtils;
+//import dji.sampleV5.modulecommon.pages;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.os.*;
+import android.view.*;
+import android.widget.Button;
+import androidx.appcompat.app.AlertDialog;
+import androidx.lifecycle.ViewModelProvider;
+import dji.sampleV5.modulecommon.R;
+import dji.sampleV5.modulecommon.models.MultiVideoChannelVM;
+import dji.sampleV5.modulecommon.models.VideoChannelVM;
+import dji.sampleV5.modulecommon.models.VideoChannelVMFactory;
+import dji.sampleV5.modulecommon.util.ToastUtils;
+import dji.v5.common.callback.CommonCallbacks;
+import dji.v5.common.error.IDJIError;
+import dji.v5.common.video.channel.VideoChannelState;
+import dji.v5.common.video.channel.VideoChannelType;
+import dji.v5.common.video.decoder.*;
+import dji.v5.common.video.interfaces.*;
+import dji.v5.common.video.stream.StreamSource;
+import dji.v5.utils.common.ContextUtil;
+import dji.v5.utils.common.DiskUtil;
+import dji.v5.utils.common.LogUtils;
+
+import android.os.Bundle;
+import android.view.*;
+import android.widget.SeekBar;
+import dji.sampleV5.modulecommon.R;
+import dji.sampleV5.modulecommon.models.VideoPlayVM;
+import dji.sampleV5.modulecommon.util.ToastUtils;
+import dji.v5.common.callback.CommonCallbacks;
+import dji.v5.common.error.IDJIError;
+import dji.v5.common.video.channel.VideoChannelType;
+import dji.v5.common.video.decoder.*;
+import dji.v5.common.video.interfaces.*;
+import dji.v5.manager.datacenter.media.MediaFile;
+import dji.v5.manager.datacenter.media.MediaManager;
+import dji.v5.manager.datacenter.media.VideoPlayState;
+import java.io.*;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import dji.sampleV5.modulecommon.R;
+import dji.sampleV5.modulecommon.models.MultiVideoChannelVM;
+import dji.sampleV5.modulecommon.util.ToastUtils;
+import dji.v5.common.video.interfaces.IVideoFrame;
+
+
 
 public class DataStreamingController {
 
@@ -150,7 +258,74 @@ public class DataStreamingController {
 
 
     public static void TestCamera(){
+        //StreamSource.
+        //Video
+        //MediaDataCenter.getInstance().videoStreamManager.addStreamSourcesListener(streamSourcesListener);
+        List<StreamSource> sources = MediaDataCenter.getInstance().getVideoStreamManager().getAvailableStreamSources();
+        List<IVideoChannel> videoChannels = MediaDataCenter.getInstance().getVideoStreamManager().getAvailableVideoChannels();
+        videoChannels.get(0).startChannel(sources.get(0), new CompletionCallback() {
+            @Override
+            public void onSuccess() {
+                Log.d("VideoStream", "sucess");
+            }
 
+            @Override
+            public void onFailure(@NonNull IDJIError error) {
+                Log.d("VideoStream", error.toString());
+            }
+        });
+
+        VideoDecoder decoder = new VideoDecoder(
+                null,
+                VideoChannelType.PRIMARY_STREAM_CHANNEL,
+                DecoderOutputMode.SURFACE_MODE, null
+        );
+        Log.d("stream", String.valueOf(decoder.getVideoHeight()));
+
+        //IVideoFrame frame = IVideoFram
+
+        //videoChannels.get(0).addStreamDataListener();
+/*
+        VideoDecoder decoder = new  VideoDecoder {
+            this @VideoPlayFragment.context,
+            VideoChannelType.PRIMARY_STREAM_CHANNEL,
+                    DecoderOutputMode.SURFACE_MODE,
+                    surfaceView.holder
+        }
+ */
+        //VideoDecoder(Context context, VideoChannelType channelType, DecoderOutputMode outputMode, Object outputSurface, int width, int height, boolean isForLiveStream)
+
+        //After decoder possible to get a frame
+    }
+
+
+    public static void TestStream(){
+        //LiveStreamVM stream = new LiveStreamVM();
+        ILiveStreamManager stream = LiveStreamManager.getInstance();
+
+        RtmpSettings.Builder rtmpBuild = new RtmpSettings.Builder();
+        rtmpBuild.setUrl("rtmp://192.168.43.11:1935"); // cia gal galima ir kitaip
+        RtmpSettings rtmpSettings = rtmpBuild.build(); // build funkcijos gal reik gal ne, nezinau ar ji po setingu ar pries turi eit
+
+        LiveStreamSettings.Builder liveStreamSettingsBuild = new LiveStreamSettings.Builder();
+        liveStreamSettingsBuild.setLiveStreamType(LiveStreamType.RTMP);
+        liveStreamSettingsBuild.setRtmpSettings(rtmpSettings);
+        LiveStreamSettings liveStreamSettings = liveStreamSettingsBuild.build();
+
+        stream.setLiveStreamSettings(liveStreamSettings);
+        //stream.se("rtmp://192.168.43.11:1935");
+
+        stream.startStream(new CompletionCallback() {
+            @Override
+            public void onSuccess() {
+                Log.d("RTMPTransmission", "Transmitting via rtmp://192.168.43.11:1935");
+            }
+
+            @Override
+            public void onFailure(@NonNull IDJIError error) {
+                Log.d("RTMPTransmission", error.toString());
+            }
+        });
     }
 
 
